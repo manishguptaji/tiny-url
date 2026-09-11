d7d47b93719abe9029d32fa8c95b5d7d2b2495eb
Manish <guptamanish181992@gmail.com>
2026-09-11 18:27:00 +0530

    added global exception class

---
+++ src/main/java/com/learn/tinyurl/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.learn.tinyurl.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .findFirst()
+                .orElse("Invalid input");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", errorMessage));
+    }
+
+    @ExceptionHandler(AliasNotAvailableException.class)
+    public ResponseEntity<Map<String, String>> handleAliasNotAvailableException(AliasNotAvailableException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
+    }
+
+}
